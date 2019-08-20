cf7b07337c2244f3275ea315b28748102d117673
chenjian <togo0915@163.com>
2019-08-20 16:17:58 +0800

    fix 添加eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-  extends: ['react-app', 'eslint:recommended'],
-  rules: {
-    "no-console": "error"
-  }
+    extends: ['react-app', 'airbnb', 'plugin:prettier/recommended'],
+    rules: {
+        "react/jsx-filename-extension": 0,
+        "no-console": 'error',
+        "indent": ["error", "tab"], //空格4个
+    },
+    // overrides: [
+    //   {
+    //     files: ['**/Mi/*.js', '**/Mi/*.jsx'],
+    //     rules: {
+    //       'react/prop-types': 'error', // Mi 文件夹下的是系统组件，必须写prop-types
+    //     },
+    //   },
+    // ],
 };
+// 'eslint:recommended'
--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css'
-let x;
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -24,33 +26,3 @@ function App() {
 }
 
 export default App;
-
-//
-// {
-//   "extends": ["airbnb", "prettier", "prettier/react"],
-//     "plugins": ["prettier"],
-//     "rules": {
-//   "react/jsx-filename-extension": [
-//     1,
-//     {
-//       "extensions": [".js", ".jsx"]
-//     }
-//   ],
-//       "react/prop-types": 0,
-//       "no-console": 2,
-//       "no-underscore-dangle": 0,
-//       "import/imports-first": ["error", "absolute-first"],
-//       "import/newline-after-import": "error"
-// },
-//   "globals": {
-//   "window": true,
-//       "document": true,
-//       "localStorage": true,
-//       "FormData": true,
-//       "FileReader": true,
-//       "Blob": true,
-//       "navigator": true
-// },
-//   "parser": "babel-eslint"
-// }
-
