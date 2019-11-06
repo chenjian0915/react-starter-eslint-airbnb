a3c0e71a226c6eb490020a2a252c7021a425d967
chenjian <togo0915@163.com>
2019-11-06 16:09:04 +0800

    change config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,24 +1,17 @@
-/*
- * @Description: In User Settings Edit
- * @Author: your name
- * @Date: 2019-08-19 10:43:13
- * @LastEditTime: 2019-08-21 08:40:38
- * @LastEditors: Please set LastEditors
- */
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
-    config
-  );
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
-  config = rewireLess.withLoaderOptions({
-    modifyVars: { "@primary-color": "#1DA57A" },
-    javascriptEnabled: true, // enable inline JavaScript in Less
-  })(config, env);
-
-  // config.output.publicPath = process.env.REACT_APP_BASENAME ? process.env.REACT_APP_BASENAME : '';
-
-  return config;
-}
+module.exports = override(
+    // 配置 antd 按需加载
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+    }),
+    // 配置less
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#1DA57A'
+        }
+    })
+);
